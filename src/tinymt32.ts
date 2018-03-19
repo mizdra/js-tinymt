3e28ec1f3c804e7bf590202de56f3c1519936bee
mizdra <pp.mizdra@gmail.com>
2018-03-20 00:47:27 +0900

    feat: implement `fromSeed`

--- src/tinymt32.ts
+++ src/tinymt32.ts
@@ -32,9 +32,38 @@ export class Rng {
 }
 
 export function fromSeed (param: Param, seed: number): Rng {
-  return new Rng(param, [])
+  const status = [
+    seed,
+    param.mat1,
+    param.mat2,
+    param.tmat,
+  ]
+  for (let i = 1; i < MIN_LOOP; i++) {
+    const a = (i) & 3
+    const b = ((i) - 1) & 3
+    status[a] ^= i + 1812433253
+      * (status[(i - 1) & 3]
+        ^ (status[(i - 1) & 3] >> 30))
+  }
+  const rng = new Rng(param, periodCertification(status))
+
+  for (let i = 0; i < PRE_LOOP; i++) {
+    rng.nextState()
+  }
+
+  return rng
 }
 
 export function fromStatus (param: Param, status: number[]): Rng {
   return new Rng(param, status.slice())
+}
+
+function periodCertification (status: number[]): number[] {
+  if ((status[0] & TINYMT32_MASK) === 0 &&
+    status[1] === 0 &&
+    status[2] === 0 &&
+    status[3] === 0) {
+    return ['T', 'I', 'N', 'Y'].map((c) => c.codePointAt(0) as number)
+  }
+  return status
 }
